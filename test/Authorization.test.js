7df606cd2a6bb9d9954f24ecda0178892ba194bd
Sam Yong <samyonggit@gmail.com>
2020-03-25 16:27:50 +0800

    update Authorization test

--- test/Authorization.test.js
+++ test/Authorization.test.js
@@ -5,14 +5,22 @@ const Authorization = require('../src/Authorization');
 test('Authorization should throw error on incorrect instantiation', () => {
   expect(() => {
     Authorization();
-  }).toThrow();
+  }).toThrow(/Invalid usage of Authorization/);
 
   expect(() => {
     new Authorization(null); /* eslint-disable-line no-new */
-  }).toThrow();
+  }).toThrow(/Invalid argument `ruleSet` given/);
 });
 
-test('RuleSet should verify correctly for given rules and context', () => {
+test('Authorization should throw error on invalid resource or access', () => {
+  expect(() => {
+    const ruleSet = new RuleSet(['users', 'groups']);
+    const authorization = new Authorization(ruleSet);
+    authorization.verify({ users: 'test', groups: 'admin' }, 'baskets', null);
+  }).toThrow(/Invalid resource or access given/);
+});
+
+test('Authorization should verify correctly for given rules and context', () => {
   const ruleSet = new RuleSet(['users', 'groups']);
   ruleSet.addRule(new Rule({ users: ['test'] }, ['products:W']));
   ruleSet.addRule(new Rule({ groups: ['admin'] }, ['products:R', 'baskets:W', 'baskets:R']));
@@ -21,12 +29,12 @@ test('RuleSet should verify correctly for given rules and context', () => {
   expect(authorization.verify({ users: 'test', groups: 'admin' }, 'baskets', 'W')).toBe(true);
   expect(authorization.verify({ users: 'test', groups: 'admin' }, 'baskets', 'R')).toBe(true);
   expect(authorization.verify({ users: 'test', groups: 'admin' }, 'products', 'W')).toBe(true);
-  expect(authorization.verify({ users: 'test', groups: 'admin' }, 'products', 'R')).toBe(true);
+  expect(authorization.verify({ users: 'test', groups: 'admin' }, 'products', 'read')).toBe(true);
 
   expect(authorization.verify({ users: 'test', groups: 'admin' }, 'products', 'X')).toBe(false);
 
   expect(authorization.verify({ users: 'test', groups: 'normalUsers' }, 'baskets', 'W')).toBe(false);
   expect(authorization.verify({ users: 'test', groups: 'normalUsers' }, 'baskets', 'R')).toBe(false);
-  expect(authorization.verify({ users: 'test', groups: 'normalUsers' }, 'products', 'W')).toBe(true);
+  expect(authorization.verify({ users: 'test', groups: 'normalUsers' }, 'products', 'write')).toBe(true);
   expect(authorization.verify({ users: 'test', groups: 'normalUsers' }, 'products', 'R')).toBe(false);
 });
