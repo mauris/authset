2fa29bbba907a2a3de33682014defebc26e4e619
Sam Yong <samyonggit@gmail.com>
2020-03-26 21:14:57 +0800

    update Authorization test cases

--- test/Authorization.test.js
+++ test/Authorization.test.js
@@ -16,8 +16,16 @@ test('Authorization should throw error on invalid resource or access', () => {
   expect(() => {
     const ruleSet = new RuleSet(['users', 'groups']);
     const authorization = new Authorization(ruleSet);
-    authorization.verify({ users: 'test', groups: 'admin' }, 'baskets', null);
+    authorization.verify({ users: 'test', groups: 'admin' }, null);
   }).toThrow(/Invalid resource or access given/);
+});
+
+test('Authorization should allow wildcards', () => {
+  const ruleSet = new RuleSet(['users', 'groups']);
+  ruleSet.addRule(new Rule({ users: ['test'] }, ['products']));
+  
+  const authorization = new Authorization(ruleSet);
+  expect(authorization.verify({ users: 'test', groups: 'admin' }, 'products:*')).toBe(true);
 });
 
 test('Authorization should verify correctly for given rules and context', () => {
@@ -26,15 +34,32 @@ test('Authorization should verify correctly for given rules and context', () => 
   ruleSet.addRule(new Rule({ groups: ['admin'] }, ['products:R', 'baskets:W', 'baskets:R']));
 
   const authorization = new Authorization(ruleSet);
-  expect(authorization.verify({ users: 'test', groups: 'admin' }, 'baskets', 'W')).toBe(true);
-  expect(authorization.verify({ users: 'test', groups: 'admin' }, 'baskets', 'R')).toBe(true);
-  expect(authorization.verify({ users: 'test', groups: 'admin' }, 'products', 'W')).toBe(true);
-  expect(authorization.verify({ users: 'test', groups: 'admin' }, 'products', 'read')).toBe(true);
+  expect(authorization.verify({ users: 'test', groups: 'admin' }, 'baskets:W')).toBe(true);
+  expect(authorization.verify({ users: 'test', groups: 'admin' }, 'baskets:R')).toBe(true);
+  expect(authorization.verify({ users: 'test', groups: 'admin' }, 'products:W')).toBe(true);
+  expect(authorization.verify({ users: 'test', groups: 'admin' }, 'products:read')).toBe(true);
 
-  expect(authorization.verify({ users: 'test', groups: 'admin' }, 'products', 'X')).toBe(false);
+  expect(authorization.verify({ users: 'test', groups: 'admin' }, 'products:X')).toBe(false);
 
-  expect(authorization.verify({ users: 'test', groups: 'normalUsers' }, 'baskets', 'W')).toBe(false);
-  expect(authorization.verify({ users: 'test', groups: 'normalUsers' }, 'baskets', 'R')).toBe(false);
-  expect(authorization.verify({ users: 'test', groups: 'normalUsers' }, 'products', 'write')).toBe(true);
-  expect(authorization.verify({ users: 'test', groups: 'normalUsers' }, 'products', 'R')).toBe(false);
+  expect(authorization.verify({ users: 'test', groups: 'normalUsers' }, 'baskets:W')).toBe(false);
+  expect(authorization.verify({ users: 'test', groups: 'normalUsers' }, 'baskets:R')).toBe(false);
+  expect(authorization.verify({ users: 'test', groups: 'normalUsers' }, 'products:write')).toBe(true);
+  expect(authorization.verify({ users: 'test', groups: 'normalUsers' }, 'products:R')).toBe(false);
+});
+
+test('Authorization should export rules correctly', () => {
+  const ruleSet = new RuleSet(['users', 'groups']);
+  ruleSet.addRule(new Rule({ users: ['test'] }, ['products']));
+  ruleSet.addRule(new Rule({ groups: ['admin'] }, ['products:R', 'baskets:W', 'baskets:R']));
+  
+  const authorization = new Authorization(ruleSet);
+  const newRule = authorization.exportRule({ users: 'test', groups: 'admin' });
+  expect(newRule).toBeInstanceOf(Rule);
+  const permissions = newRule.getPermissions();
+  expect(permissions).toBeInstanceOf(Set);
+  expect(permissions.size).toBe(4);
+  expect(permissions).toContain('products:*');
+  expect(permissions).toContain('products:R');
+  expect(permissions).toContain('baskets:W');
+  expect(permissions).toContain('baskets:R');
 });
